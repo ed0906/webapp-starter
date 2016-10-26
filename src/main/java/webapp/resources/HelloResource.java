39da553a9eefb41107d4549917fb6ccff1d3808f
Edward Wilson <edward.wilson@autotrader.co.uk>
2016-10-26 15:44:37 +0100

    Make hello resource singleton

--- src/main/java/webapp/resources/HelloResource.java
+++ src/main/java/webapp/resources/HelloResource.java
@@ -1,11 +1,13 @@
 package webapp.resources;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/")
+@Singleton
 public class HelloResource {
 
     @GET
